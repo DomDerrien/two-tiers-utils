d29979995b8c52cbf1c4bf9f07c08ebfb4121bc8
Dom Derrien <dominique.derrien@gmail.com>
2009-09-23 14:39:34 -0400

    addition of a mock class for ServletInputStream

---
+++ src/Java/domderrien/mocks/MockServletInputStream.java
@@ -0,0 +1,49 @@
+package domderrien.mocks;
+
+import java.io.IOException;
+
+import javax.servlet.ServletInputStream;
+
+public class MockServletInputStream extends ServletInputStream {
+    private StringBuilder stream;
+    private int cursor = 0;
+    private int limit;
+
+    /** Default constructor. Use <code>reset(String)</code> to set the stream content. */
+    public MockServletInputStream() {
+        setData("");
+    }
+    /** Constructor with the initial stream content */
+    public MockServletInputStream(String data) {
+        setData(data);
+    }
+
+    /** Accessor */
+    public void setData(String data) {
+        stream = new StringBuilder(data);
+        limit = stream.length();
+    }
+
+    @Override
+    public int read() throws IOException {
+        if(cursor < limit) {
+            char c = stream.charAt(cursor);
+            ++ cursor;
+            return (int) c;
+        }
+        return -1;
+    }
+
+    /** Return the initial stream content */
+    public String getContents() {
+        return stream.toString();
+    }
+    /** Return the yet processed stream content */
+    public String getProcessedContents() {
+        return stream.substring(0, cursor - 1);
+    }
+    /** Return the non yet processed stream content */
+    public String getNotProcessedContents() {
+        return stream.substring(cursor);
+    }
+};
--- src/WebContent/js/domderrien/i18n/nls/fr/domderrien-labels.js
+++ src/WebContent/js/domderrien/i18n/nls/fr/domderrien-labels.js
@@ -1,3 +1,3 @@
 ({bundle_language:"Français",
 unit_test_sample:"N/A",
-x_timeStamp:"20090917.1031"})
+x_timeStamp:"20090923.1435"})
