894908bad725c506cffb73158f2cf4fd8710871b
unknown <dominique.derrien@gmail.com>
2009-10-07 09:26:05 -0400

    restore files removed because of a merge issue ;)

---
+++ src/WebContent/js/domderrien/i18n/nls/en/domderrien-labels.js
@@ -0,0 +1,4 @@
+({bundle_language:"English",
+unit_test_sample:"N/A",
+unit_test_sample_with_parameters:"whatever you want ${0}, and even more ${1}",
+x_timeStamp:"20091007.0912"})
---
+++ src/WebContent/js/domderrien/i18n/nls/fr-ca/domderrien-labels.js
@@ -0,0 +1,4 @@
+({bundle_language:"Français Canadien",
+unit_test_sample:"N/A",
+unit_test_sample_with_parameters:"tout ce que vous voulez ${0}, et même plus ${1}",
+x_timeStamp:"20091007.0914"})
