a78f2c4284a099e5e8440be1de8843ee6a3197ef
Dom Derrien <dominique.derrien@gmail.com>
2009-11-18 21:06:49 -0500

    repackage because of 1 missed file...

--- src/WebContent/js/domderrien/i18n/nls/en/domderrien-labels.js
+++ src/WebContent/js/domderrien/i18n/nls/en/domderrien-labels.js
@@ -2,4 +2,4 @@
 unit_test_sample:"N/A",
 unit_test_sample_with_parameters:"whatever you want ${0}, and even more ${1}",
 unit_test_sample_with_text_on_many_lines:"As a writer, it's possible I'm going to write a TMX entry on many lines, especially if it's the content of an help message where the content should be as clear as possible. Gotcha?",
-x_timeStamp:"20091118.2030"})
+x_timeStamp:"20091118.2102"})
