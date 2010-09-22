f0ed95e1b8c58de37e93c68e5a5cc5e2dc278873
Dom Derrien <dominique.derrien@gmail.com>
2010-09-22 09:09:24 -0400

    Syntax fix after gjslint run
    Signed-off-by: Dom Derrien <dominique.derrien@gmail.com>

--- src/WebContent/js/domderrien/i18n/LanguageSelector.js
+++ src/WebContent/js/domderrien/i18n/LanguageSelector.js
@@ -2,21 +2,19 @@
 
     /**
      * @author dom.derrien
-     * @maintainer dom.derrien
      */
     var module = dojo.provide("domderrien.i18n.LanguageSelector");
 
-    dojo.require("dojo.data.ItemFileReadStore");
     dojo.require("dijit.form.Select");
 
     /**
-     * Attach a <code>FilteringSelect</code> widget to the identified
+     * Attach a <code>Select</code> widget to the identified
      * DOM node. The widget <code>onChange</code> is connected to a
      * function refreshing the page with the identifier of the newly
      * selected language.
      *
      * @param {String} placeHolderId Identifier of the DOM that will be
-     * replaced by the <code>FilteringSelect</code> widget.
+     * replaced by the <code>Select</code> widget.
      *
      * @param {String} formFieldName Value of the attribute <code>name</code>
      * for the underneath HTML element <code>&lt;select/&gt;</code>.
@@ -26,9 +24,9 @@
      * identify the language ISO code and the localized language label.
      *
      * @param {String} currentLanguageId ISO code of the language
-     * to be initially selected
+     * to be initially selected.
      *
-     * @param {String} className Optional CSS class name for the widget to be created
+     * @param {String} className Optional CSS class name for the widget to be created.
      *
      * @param {Function} onChangeHandler Optional event handler attached
      * to the <code>onChange</code> event that the selector can generate. If
@@ -36,7 +34,8 @@
      * with the attribute <code>lang=&ltselected-value&gt;</code>.
      *
      */
-    module.createSelector = function(placeHolderId, formFieldName, options, currentLanguageId, className, onChangeHandler){
+    module.createSelector = function(placeHolderId, formFieldName, options, currentLanguageId, className, onChangeHandler) {
+        _currentLanguageId = currentLanguageId;
         var _selector = new dijit.form.Select({
                 autoComplete: false,
                 'class': className || "",
@@ -47,28 +46,36 @@
             },
             placeHolderId
         );
-        _selector.attr("value", currentLanguageId);
+        _selector.set("value", currentLanguageId);
         dojo.connect(_selector, "onChange", _selector, onChangeHandler || _switchLanguage);
         return _selector;
     };
 
+    var _currentLanguageId;
+
     /**
      * Event handler attach to the selector language and which is updating the
-     * page URL with the identifier of the selected language ISO code
+     * page URL with the identifier of the selected language ISO code.
      *
      * @private
      */
     var _switchLanguage = function() {
         // Not testable because it's going to change the window content and then will break the test environment
-        window.location.search = module._replaceLanguage(window.location.search, this.attr("value"));
+        var out = module._replaceLanguage(window.location.search, this.attr("value"));
+        if (out) {
+            window.location.search = out;
+        }
     };
 
     // Just provided to be able to control the environment during the unit tests
     module._replaceLanguage = function(source, newLanguage) {
-        if (source.indexOf("lang=") == -1) {
-            return source + (source.length == 0 ? "?" : "&") + "lang=" + newLanguage;
+        if (_currentLanguageId != newLanguage) {
+            if (source.indexOf("lang=") == -1) {
+                return source + (source.length == 0 ? "?" : "&") + "lang=" + newLanguage;
+            }
+            return source.replace(/lang=((\w\w_\w\w)|(\w\w))/g, "lang=" + newLanguage);
         }
-        return source.replace(/lang=((\w\w_\w\w)|(\w\w))/g, "lang=" + newLanguage);
+        return null;
     };
 
 })(); // End of the function limiting the scope of the private variables
